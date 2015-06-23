0f31abcbf5a7b3d61354da1415639c70894248e8
warmans <stefan.warman@switchconcepts.com>
2015-06-23 20:11:03 +0100

    Added comment

--- report.go
+++ report.go
@@ -38,8 +38,8 @@ func (t *TextReport) Print(res *Result) {
 	fmt.Printf("Imports:       %v\n", res.Import)
 	fmt.Printf("Structs:       %v\n", res.Struct)
 	fmt.Printf("Interfaces:    %v\n", res.Interface)
-	fmt.Printf("Methods:       %v (%v Exported, %v LOC, %v LOC Avg.)\n", res.Method, res.ExportedMethod, res.MethodLOC, t.avg(res.MethodLOC, res.Method))
-	fmt.Printf("Functions:     %v (%v Exported, %v LOC, %v LOC Avg.)\n", res.Function, res.ExportedFunction, res.FunctionLOC, t.avg(res.FunctionLOC, res.Function))
+	fmt.Printf("Methods:       %v (%v Exported, %v LOC, %v LOC Avg.)\n", res.Method, res.ExportedMethod, res.MethodLOC, t.divideBy(res.MethodLOC, res.Method))
+	fmt.Printf("Functions:     %v (%v Exported, %v LOC, %v LOC Avg.)\n", res.Function, res.ExportedFunction, res.FunctionLOC, t.divideBy(res.FunctionLOC, res.Function))
 	fmt.Println(strings.Repeat("-", 80))
 	fmt.Printf("Ifs:           %v \n", res.IfStatement)
 	fmt.Printf("Switches:      %v \n", res.IfStatement)
@@ -51,7 +51,8 @@ func (t *TextReport) Print(res *Result) {
 	fmt.Printf("\n")
 }
 
-func (t *TextReport) avg(num, dividedBy int) int {
+//Safely divide where vivisor might be zero
+func (t *TextReport) divideBy(num, dividedBy int) int {
 	if dividedBy == 0 {
 		return 0
 	}
